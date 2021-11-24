69cb2dd988495e3e251af765c76902c9e02ba982
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-24 15:51:20 +0000

    implement up method to increase temp by 2

--- thermostat.test.js
+++ thermostat.test.js
@@ -5,4 +5,9 @@ describe("Thermostat class", () => {
     const thermostat = new Thermostat();
     expect(thermostat.getTemperature()).toBe(20);
   });
+  it("returns an increased temperature", () => {
+    const thermostat = new Thermostat();
+    thermostat.up();
+    expect(thermostat.getTemperature()).toBe(22);
+  });
 });
